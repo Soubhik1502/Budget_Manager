05ed7cc491fac7ffe969554a921d6d394b4c9e34
Soubhik Chakraborty <soubhik560@gmail.com>
2024-05-19 13:08:04 +0530

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" ><h4>Personalized Finance</h4><h5>Secure</h5></a>
+                   
+                   
+                </div>
+            </nav>
+        </div>
+    )
+}
